cbda6827179943e5b161f0b72cf22cfd04cd4b42
Jillian Österreich <contact@lumaeris.com>
2025-03-10 15:13:38 +0700

    chore: remove leftover script

--- files/scripts/librewolfnativemessaging.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-set -oue pipefail
-
-mkdir -p /usr/lib/librewolf
-ln -s /usr/lib/mozilla/native-messaging-hosts /usr/lib/librewolf/native-messaging-hosts
