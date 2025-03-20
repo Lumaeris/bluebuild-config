4c00603337f99f8ceea619d80d3e4dc5e242e730
Jillian Österreich <contact@lumaeris.com>
2025-03-20 17:33:08 +0300

    chore(deck): fixup changeublueimageinfo.sh script

--- files/scripts/changeublueimageinfo.sh
+++ files/scripts/changeublueimageinfo.sh
@@ -6,5 +6,4 @@ set -oue pipefail
 
 # tell ublue-update that it should check updates from my image, NOT the official ones
 IMAGE_REF="ostree-image-signed:docker://ghcr.io/lumaeris/bazzite-deck"
-printf '{\n"image-ref": "'"$IMAGE_REF"'",\n"image-tag": "latest",\n"image-name": "bazzite-deck"\n,"base-image-name": "kinoite"\n}' > /usr/share/ublue-os/image-info.json
-sed -i 's/image-branch/image-tag/' /usr/libexec/bazzite-fetch-image
+printf '{\n"image-ref": "'"$IMAGE_REF"'",\n"image-tag": "latest",\n"image-name": "bazzite-deck",\n"base-image-name": "kinoite",\n"image-branch": "latest"\n}' > /usr/share/ublue-os/image-info.json
