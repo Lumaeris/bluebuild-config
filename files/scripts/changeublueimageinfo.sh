9c745f8bce47442ec3251c5bdbe5b521fdaf6d79
Jillian Österreich <contact@lumaeris.com>
2025-03-20 21:53:19 +0300

    fix(deck): HOPEFULLY fix updating through steam's gaming mode
    kinda tired

--- files/scripts/changeublueimageinfo.sh
+++ files/scripts/changeublueimageinfo.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
-# thx to https://github.com/HikariKnight/Deckstation/blob/main/files/scripts/main/deckstation-setup.sh for this hacky fix!
 
 # Tell this script to exit if there are any errors.
 set -oue pipefail
 
 # tell ublue-update that it should check updates from my image, NOT the official ones
-IMAGE_REF="ostree-image-signed:docker://ghcr.io/lumaeris/bazzite-deck"
-printf '{\n"image-ref": "'"$IMAGE_REF"'",\n"image-tag": "latest",\n"image-name": "bazzite-deck",\n"base-image-name": "kinoite",\n"image-branch": "latest",\n"image-flavor": "deck",\n"image-vendor": "lumaeris"\n}' > /usr/share/ublue-os/image-info.json
+cat > /usr/share/ublue-os/image-info.json <<EOF
+{
+  "image-name": "bazzite-deck",
+  "image-flavor": "deck",
+  "image-vendor": "lumaeris",
+  "image-ref": "ostree-image-signed:docker://ghcr.io/lumaeris/bazzite-deck",
+  "image-tag": "latest",
+  "image-branch": "latest",
+  "base-image-name": "kinoite",
+  "fedora-version": "41"
+}
+EOF
