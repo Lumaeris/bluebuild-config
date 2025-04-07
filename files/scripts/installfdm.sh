7e13130d46a27774dd077e719da8750b6f18d407
Jillian Österreich <contact@lumaeris.com>
2025-04-07 15:18:04 +0300

    chore(main): Fix FDM installation

--- files/scripts/installfdm.sh
+++ files/scripts/installfdm.sh
@@ -10,5 +10,6 @@ wget https://files2.freedownloadmanager.org/6/latest/freedownloadmanager.deb
 ar x freedownloadmanager.deb
 tar -xvJf data.tar.xz -C /
 sed -i '/^Exec=/ s/^Exec=/&env QT_QPA_PLATFORM=xcb /' /usr/share/applications/freedownloadmanager.desktop
+mv /opt/freedownloadmanager /usr/lib/opt/
 popd > /dev/null
 rm -rf "${dir}"
