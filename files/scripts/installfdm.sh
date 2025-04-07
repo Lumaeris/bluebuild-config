7243008df151c09607102c251f32c7d3426aa0ac
Jillian Österreich <contact@lumaeris.com>
2025-04-07 12:26:19 +0300

    feat(main): Add FDM

---
+++ files/scripts/installfdm.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+set -oue pipefail
+
+pushd . > /dev/null
+dir="$(mktemp -d)"
+cd "${dir}"
+wget https://files2.freedownloadmanager.org/6/latest/freedownloadmanager.deb
+ar x freedownloadmanager.deb
+tar -xvJf data.tar.xz -C /
+sed -i '/^Exec=/ s/^Exec=/&env QT_QPA_PLATFORM=xcb /' /usr/share/applications/freedownloadmanager.desktop
+popd > /dev/null
+rm -rf "${dir}"
