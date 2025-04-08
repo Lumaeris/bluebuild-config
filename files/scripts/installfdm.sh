1b14f5e390d6604da9614b929186f1427a2f48f2
Jillian Österreich <contact@lumaeris.com>
2025-04-08 16:10:25 +0300

    fix(main): Do NOT create /opt/opt
    let's see if it works

--- files/scripts/installfdm.sh
+++ files/scripts/installfdm.sh
@@ -8,8 +8,15 @@ dir="$(mktemp -d)"
 cd "${dir}"
 wget https://files2.freedownloadmanager.org/6/latest/freedownloadmanager.deb
 ar x freedownloadmanager.deb
+if [ ! -f '/opt' ]; then
+    OPTF=1
+fi
 tar -xvJf data.tar.xz -C /
 sed -i '/^Exec=/ s/^Exec=/&env QT_QPA_PLATFORM=xcb /' /usr/share/applications/freedownloadmanager.desktop
-mv /opt/freedownloadmanager /usr/lib/opt/
+if [ "${OPTF}" -eq 1 ]; then
+    mv /opt /usr/lib/
+else
+    mv /opt/freedownloadmanager /usr/lib/opt/
+fi
 popd > /dev/null
 rm -rf "${dir}"
