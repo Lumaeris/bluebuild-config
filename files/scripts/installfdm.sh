9d27357f504d38db5455733b3301539451e70fd8
Jillian Österreich <contact@lumaeris.com>
2025-04-08 16:19:08 +0300

    chore(main): Handle FDM installation better

--- files/scripts/installfdm.sh
+++ files/scripts/installfdm.sh
@@ -8,15 +8,9 @@ dir="$(mktemp -d)"
 cd "${dir}"
 wget https://files2.freedownloadmanager.org/6/latest/freedownloadmanager.deb
 ar x freedownloadmanager.deb
-if [ ! -f '/opt' ]; then
-    OPTF=1
-fi
-tar -xvJf data.tar.xz -C /
+tar -xvJf data.tar.xz
+cp -R ./usr/* /usr/
+cp -R ./opt/* /usr/lib/opt/
 sed -i '/^Exec=/ s/^Exec=/&env QT_QPA_PLATFORM=xcb /' /usr/share/applications/freedownloadmanager.desktop
-if [ "${OPTF}" -eq 1 ]; then
-    mv /opt /usr/lib/
-else
-    mv /opt/freedownloadmanager /usr/lib/opt/
-fi
 popd > /dev/null
 rm -rf "${dir}"
